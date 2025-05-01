8a64252976955b8513417161f620103bfa8e6161
hemanthkt <hmkt1503@gmail.com>
2025-05-01 15:17:12 +0530

    ping pong

--- src/index.ts
+++ src/index.ts
@@ -4,14 +4,16 @@ const wss = new WebSocketServer({port: 8080})
 
 wss.on("connection", function(socket) {
     console.log("user connected");
-
-    setInterval (() => {
-
-        socket.send("price of solana" + Math.random())
-    },1000)
-    
     socket.on("message", (e) => {
         console.log(e.toString());
+
+        if (e.toString() === "ping") {
+         
+
+            console.log("Ping received!");
+            socket.send("pong");
+        }
+        
         
     })
     
