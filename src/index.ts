cb2517820fc23f32b775db52634122864da28d24
hemanthkt <hmkt1503@gmail.com>
2025-05-01 13:27:45 +0530

    connectio implementation

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,18 @@
-console.log("hi");
+import { WebSocketServer } from "ws";
+
+const wss = new WebSocketServer({port: 8080})
+
+wss.on("connection", function(socket) {
+    console.log("user connected");
+
+    setInterval (() => {
+
+        socket.send("price of solana" + Math.random())
+    },1000)
+    
+    socket.on("message", (e) => {
+        console.log(e.toString());
+        
+    })
+    
+})
